e9353d1c5aba0f19331b551808bf20fcb2141186
Miko Gonzaga <14193267+miikogonzaga@users.noreply.github.com>
2021-10-24 19:47:46 -0700

    add mongoose & create post model

--- backend/app.js
+++ backend/app.js
@@ -1,49 +1,63 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://admin:8zNZXyNRuAPHvs3y@cluster0.zjvoj.mongodb.net/ng-app?retryWrites=true&w=majority")
+    .then(() => {
+        console.log('Connected to database')
+    })
+    .catch(() => {
+        console.log('Connection failed')
+    })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
-  });
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    console.log(post);
+    post.save()
+    res.status(201).json({
+        message: 'Post added successfully'
+    });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
-  });
+    const posts = [{
+            id: "fadf12421l",
+            title: "First server-side post",
+            content: "This is coming from the server"
+        },
+        {
+            id: "ksajflaj132",
+            title: "Second server-side post",
+            content: "This is coming from the server!"
+        }
+    ];
+    res.status(200).json({
+        message: "Posts fetched successfully!",
+        posts: posts
+    });
 });
 
 module.exports = app;
